227ced61db59c9c4eb0cf107a34fe3d558662ca6
kemarCL <kemcode2013@gmail.com>
2016-05-30 14:41:42 -0500

    Add files via upload

---
+++ backend.php
@@ -0,0 +1,44 @@
+<?php
+/*
+   Backend recieves an email then verifies it with emailhunter
+   Response is then stored in a file and appropriate response sent back to the frontend
+   Kemar Clarke
+
+*/
+
+//Code below checks if and email was posted 
+if (isset($_POST["email"])) 
+{
+
+    //setting up cUrl request to be sent to the emailhunter API
+    $handle =curl_init();
+    curl_setopt($handle,CURLOPT_URL ,'https://api.emailhunter.co/v1/verify?email='.$_POST["email"].'&api_key=2882d3c37060ca2364247dbb15e51cd002dc408a');
+    curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+
+    //execute the cUrl request and store the result
+    $response = curl_exec($handle);
+    curl_close($handle);
+
+
+     //code below prepares or create the file to write the response to
+
+    $ini= fopen('results.txt', 'a');
+
+    fwrite($ini, $response);
+
+    fclose($ini);
+
+
+     //decoding the JSON response to send back to the frontend
+    $final = json_decode($response,true);
+    $fr=$final['result'];
+
+    //retuning a suitable response to the front end
+    echo $fr;
+
+}
+
+
+?>
+
+                  
